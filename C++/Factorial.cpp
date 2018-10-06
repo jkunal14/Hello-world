97649549d84771cdabe3c33eba8ccba2c597def4
jkunal14 <43882559+jkunal14@users.noreply.github.com>
2018-10-06 22:22:05 +0530

    Update Factorial.cpp

--- C++/Factorial.cpp
+++ C++/Factorial.cpp
@@ -3,18 +3,27 @@ using namespace std;
 
 int factorial(int n)
 {
-    if(n > 1)
-        return n * factorial(n - 1);
+    if(n<0)
+    {
+        return(-1); //Corresponds to wrong value
+    }
+    if(n==0)
+    {
+        return(1); //Terminating condition
+    }
     else
-        return 1;
-
+    {
+        return(n*factorial(n-1)); //recursive call
+    }
 }
 int main()
 {
-    int n;
-    cout << "Enter a positive integer: ";
-    cin >> n;
-    cout << "Factorial of " << n << " = " << factorial(n);
+    int factorial(int);
+    int fact,n;
+    cout<<"Enter the number of which u wish to get factorial"<<endl;
+    cin>>n;
+    fact=factorial(n);
+    cout<<"Factorial of the number you entered is "<<fact<<endl;
     return 0;
 }
 
